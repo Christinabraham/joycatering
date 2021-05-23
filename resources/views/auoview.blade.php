91e0871b6159958e478b1f1346805645e37a4e9d
Christinabraham <mca514@rajagiri.edu>
2021-05-23 22:29:58 +0530

    m

--- resources/views/auoview.blade.php
+++ resources/views/auoview.blade.php
@@ -6,10 +6,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>view</title>
+    <title id="hide">orders</title>
 </head>
 
 <body background="https://wallpaperaccess.com/full/1566582.jpg"  style="height: 100%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+<style type="text/css">
+@media print {
+    #hide {
+        display :  none;
+    }
+}
+</style>
     <nav class="navbar navbar-expand-lg navbar-style="color:rgba(247, 247, 248, 0.911) bg-dark">
         <div class="container-fluid" style="font-family:courier;">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
@@ -60,7 +67,7 @@
     <th >Stage Design</th>
     <th >Total Price</th>
     
-    <th></th> <th></th>
+    
     </tr>
 
 @foreach($prod as $prods)
@@ -80,19 +87,21 @@
 @endforeach
 </table>
 </div>
- <div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2">
+<div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2">
+<button id="hide" class="btn btn-outline-warning" onclick="window.print()">Print</button>
+ 
 </div>
 </div>
 </div>
 <div class="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
     <br>
     <br>
-    <h6 style="font-family:verdana;color:rgb(151, 155, 151);">USEFUL LINKS</h6>
+    <h6 id="hide" style="font-family:verdana;color:rgb(151, 155, 151);">USEFUL LINKS</h6>
     <table class="table table-borderless">
-        <tr><td><a class="navbar-brand" href="/ah">Home</a></td></tr>  
+        <tr><td><a id="hide" class="navbar-brand" href="/ah">Home</a></td></tr>  
                
-                <tr><td><a class="navbar-brand" href="/ai">Additem</a></td></tr>
-                <tr><td><a class="navbar-brand" href="/userview">View</a></td></tr>
+                <tr><td><a id="hide" class="navbar-brand" href="/ai">Additem</a></td></tr>
+                <tr><td><a id="hide" class="navbar-brand" href="/userview">View</a></td></tr>
     </table>
         </div>
 
